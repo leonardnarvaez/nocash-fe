9135563394768519b60e254cca0868bf734e0f0d
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-07-03 17:58:15 +0800

    Added error message in User Registration failure

--- src/app/content-pages/register/register.component.ts
+++ src/app/content-pages/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -41,9 +43,22 @@ export class RegisterComponent implements OnInit{
     }
     const {emailAddress, mobileNumber, pin} = this.registerForm.value;
     this.authService.register(emailAddress, mobileNumber, pin)
+    .pipe(catchError(this.handleError))
     .subscribe((user: User) => {
       this.router.navigateByUrl("/login");
-      
     })
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) 
+    {
+      console.error('An error occurred:', error.error);
+    }
+    else 
+    {
+      console.warn(error);
+      alert(error.error.message);
+    }
+    return throwError(() => new Error('HAHAHAHAHA'))
   }
 }
