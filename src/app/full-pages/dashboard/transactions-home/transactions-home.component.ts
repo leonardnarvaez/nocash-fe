fd6983cda529f60eaa50eaa629fe2f72fafe7b2c
jonnliwagg <jonsnoooww@gmail.com>
2023-07-04 08:19:29 +0800

    redirected to homepage after logging in, fixed ui components for homepage

---
+++ src/app/full-pages/dashboard/transactions-home/transactions-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'transactions-home',
+  templateUrl: './transactions-home.component.html',
+  styleUrls: ['./transactions-home.component.css']
+})
+export class TransactionsHomeComponent {
+
+}
