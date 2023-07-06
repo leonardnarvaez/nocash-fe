c1b4be12f6f99b5f172b9773f5705145a07d39bc
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-07-06 17:42:38 +0800

    Adjusted routerLink for merchant list in home page

--- src/app/full-pages/home/home.component.ts
+++ src/app/full-pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  walletBalance: number;
+  urlEndPoint = "http://localhost:8080/api/card-transaction/balance"
+  constructor(
+    private http: HttpClient
+  ) {
+    this.walletBalance = 0;
+  }
+  ngOnInit(): void {
+    this.http.get<number>(this.urlEndPoint)
+    .subscribe((balance: number) => {
+      this.walletBalance = balance;
+    });
+  }
 
+  
 }
