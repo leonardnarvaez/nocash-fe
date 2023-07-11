1be0bf869efe413d9272bc2168a0f7b6b7525a76
jonnliwagg <jonsnoooww@gmail.com>
2023-07-11 17:58:45 +0800

    pin validation for pay bill form

--- src/app/full-pages/bill-payment-form/bill-payment-form.component.ts
+++ src/app/full-pages/bill-payment-form/bill-payment-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/app/environments/environment';
 import { Location } from '@angular/common';
+import { catchError, throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-bill-payment-form',
@@ -26,7 +28,7 @@ export class BillPaymentFormComponent {
     this.billPaymentForm = this.formBuilder.group(
       {
         accountNumber: ['', [Validators.required]],
-        amount: ['', [Validators.required]],
+        amount: ['', [Validators.required, this.positiveBalanceValidator]],
         pin: ['',  [Validators.required, Validators.minLength(4), Validators.maxLength(4)]]
       }
     )
@@ -43,17 +45,41 @@ export class BillPaymentFormComponent {
     const payload = {
       amount: this.billPaymentForm.get('amount')?.value,
       merchantId: this.merchantId,
-      accountNumber: this.billPaymentForm.get('accountNumber')?.value
+      accountNumber: this.billPaymentForm.get('accountNumber')?.value,
+      pin: this.billPaymentForm.get('pin')?.value
     }
-    this.http.post<HttpResponse<any>>(this.urlEndPoint, payload).subscribe(
+    this.http.post<HttpResponse<any>>(this.urlEndPoint, payload).pipe(
+      catchError(this.handleError)
+    ).subscribe(
       (response: HttpResponse<any>) => {
         console.log(response)
+        this.router.navigateByUrl('/app/home')
       }
     )
-    this.router.navigateByUrl('/app/home')
   }
   
   goBack(): void {
     this.location.back();
+  }
+
+  positiveBalanceValidator(control: AbstractControl) {
+    const amount = control.value;
+    if (amount < 0) {
+      return { negative: true };
+    }
+    return null;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) 
+    {
+      console.error('An error occurred:', error.error);
+    }
+    else 
+    {
+      console.warn(error);
+      alert(error.error.message);
+    }
+    return throwError(() => new Error(''))
   }
 }
