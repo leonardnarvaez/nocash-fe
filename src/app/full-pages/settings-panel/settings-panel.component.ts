fcd205fc670b5a5c454f78c76b0f674c4e167a63
jonnliwagg <jonsnoooww@gmail.com>
2023-07-12 15:11:38 +0800

    confirmation for logout dialog box

--- src/app/full-pages/settings-panel/settings-panel.component.ts
+++ src/app/full-pages/settings-panel/settings-panel.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/app/environments/environment';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-settings-panel',
@@ -12,10 +13,12 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SettingsPanelComponent implements OnInit{
   base_url;
+  dialogRef!: MatDialogRef<LogoutConfirmationDialogComponent>;
   constructor(
     private httpClient: HttpClient,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
     ){
     this.base_url = environment.API_HOST;
   }
@@ -27,11 +30,35 @@ export class SettingsPanelComponent implements OnInit{
   }
   logout() {
     console.log('~~ logout');
-    
+    try {
+      this.dialogRef.close();
+    } catch(e) {
+      console.log(e);
+      
+    }
+    this.dialogRef = this.dialog.open(LogoutConfirmationDialogComponent, {
+      width: '250px',
+      data: {
+        message: 'Are you sure you want to logout?',
+      },
+    });
+    this.dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
     this.authService.logout().subscribe(m => {
       console.log(m);
       this.router.navigateByUrl('/login');
     });
-    
   }
+  })
+}
+}
+@Component({
+  selector: 'app-logout-confirmation-dialog',
+  templateUrl: 'logout-confirmation-dialog.html',
+})
+export class LogoutConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<LogoutConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 }
